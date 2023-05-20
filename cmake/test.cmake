f411d6aff127579c6a19d870fd2a351ece8a7a38
Bartosz Myrcha <bartosz.myrcha@intel.com>
2023-05-20 18:46:59 +0200

    Update test.cmake

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -13,6 +13,44 @@ include(GoogleTest)
 set(CTEST test/csrc)
 file(GLOB_RECURSE ALL_TESTS ${CTEST}/*.cpp)
 
+set(WITH_MKL_BLAS 0)
+if(USE_MKL_BLAS AND DEFINED BLAS_INCLUDE_DIR)
+  message(WARNING "YAAAAAAAAAAAAAAAAAAAAAAAY!")
+  find_file(MKL_INCLUDE_FOUND mkl.h ${BLAS_INCLUDE_DIR} NO_DEFAULT_PATH)
+  find_path(MKL_INCLUDE_DIR mkl.h ${BLAS_INCLUDE_DIR} NO_CACHE)
+  if(MKL_INCLUDE_FOUND)
+    message(WARNING "We found MKL! YAAAAAAAAAAAAAAAAAAAAAAAY v2!")
+    message(WARNING "We found MKL: ${MKL_INCLUDE_FOUND}")
+    # set(MKL_INCLUDE_DIR /opt/hostedtoolcache/Python/3.8.16/x64/include)
+    
+    message(WARNING "Including MKL DIR: ${MKL_INCLUDE_DIR}")
+    
+    include_directories(${MKL_INCLUDE_DIR})
+    target_include_directories(${PROJECT_NAME} PUBLIC ${MKL_INCLUDE_DIR} NO_CACHE)
+
+    # Link the required MKL libraries
+    
+   # find_library(MKL_LIB mkl_intel_lp64)
+   # find_library(MKL_CORE_LIB mkl_core)
+   # find_library(MKL_THREAD_LIB mkl_gnu_thread)
+
+   # target_link_libraries(${PROJECT_NAME} PRIVATE ${MKL_LIB} ${MKL_CORE_LIB} ${MKL_THREAD_LIB})
+    set(WITH_MKL_BLAS 1)
+  else()
+    if(WITH_COV)
+      message(FATAL_ERROR "The mkl.h file was not found - pass the correct directory or set USE_MKL_BLAS=OFF.")
+    else()
+      message(WARNING "The mkl.h file was not found - building pyg-lib without MKL BLAS support.")
+    endif()
+  endif()
+endif()
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/pyg_lib/csrc/config.h.in "${CMAKE_CURRENT_SOURCE_DIR}/pyg_lib/csrc/config.h")
+
+if(MKL_INCLUDE_FOUND)
+    target_include_directories(${PROJECT_NAME} PUBLIC ${BLAS_INCLUDE_DIR})
+    message(WARNING "Including directory: ${BLAS_INCLUDE_DIR}")
+endif()
+
 foreach(test ${ALL_TESTS})
     get_filename_component(name ${test} NAME_WE)
     add_executable(${name} ${test})
